7adbf3a37d7290842e47a726bd710d065d681f92
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-12-03 01:56:01 -0500

    configured vite to serve the base as the repo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/React-Portfolio/",
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.PNG', '**/*.png'],
   server: {
     port: 3000,
